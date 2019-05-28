6df7a05dd7222f0bdd45792cc6a2c9c42767b5c9
2019wei <linkoko520@gmail.com>
2019-05-29 00:50:19 +0800

    app

---
+++ Bmi/app/src/main/java/com/tom/bmi/hello/Person.java
@@ -0,0 +1,34 @@
+package com.tom.bmi.hello;
+
+public class Person {
+
+
+    float weight;
+    float height;
+    public float bmi(){
+        float bmi = weight / (height*height);
+        return bmi;
+    }
+    public void hello(){
+        System.out.println("hello");
+    }
+    public  void  hello(String name){
+        System.out.println(name + ".hello");
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
