b4525e6004d825251303ce3223f0e00a6c28b2a8
2019wei <linkoko520@gmail.com>
2019-09-09 00:30:11 +0800

    update

--- homework/src/main/java/com/train/Tester.java
+++ homework/src/main/java/com/train/Tester.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Please enter number of tickets: ");
-        Scanner scanner = new Scanner(System.in);
-        Integer ticket =  scanner.nextInt();
-        System.out.println("How many round-trip tickets:  ");
-        Integer goback =  scanner.nextInt();
-        Purchase purchase = new Purchase(ticket,goback);
-        purchase.print();
-
+        int ticket;
+        do {
+            System.out.println("Please enter number of tickets: ");
+            Scanner scanner = new Scanner(System.in);
+            ticket = scanner.nextInt();
+            if(ticket != -1) {
+                System.out.println("How many round-trip tickets:  ");
+                Integer goback = scanner.nextInt();
+                Purchase purchase = new Purchase(ticket, goback);
+                purchase.print();
+            }
+        }
+        while (ticket != -1);
+        System.out.println("finish");
 
     }
 }
