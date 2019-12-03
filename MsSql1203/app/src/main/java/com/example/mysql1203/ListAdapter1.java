b713bf057e47de33e3824460623e8aa50bb92050
wei2019 <linkoko520@gmail.com>
2019-12-03 15:05:16 +0800

    12/3 MSSQL測試

---
+++ MsSql1203/app/src/main/java/com/example/mysql1203/ListAdapter1.java
@@ -0,0 +1,47 @@
+package com.example.mysql1203;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListAdapter1 extends BaseAdapter {
+
+
+    ArrayList<String> list = new ArrayList<>();
+
+    public ListAdapter1(ArrayList list) {
+        this.list = list;
+    }
+
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_view,parent,false);
+        }
+        String str = list.get(position);
+        TextView t1 = convertView.findViewById(R.id.t1);
+        t1.setText(str);
+
+        return convertView;
+    }
+}
