bc82d5ccf55502c9d521ef44f4bf992d304ccbd5
wei2019 <linkoko520@gmail.com>
2020-03-11 17:01:27 +0800

    0311

--- Atm22/app/src/main/java/com/tom/atm22/FinanceActivity.java
+++ Atm22/app/src/main/java/com/tom/atm22/FinanceActivity.java
@@ -5,7 +5,10 @@ import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.database.FirebaseDatabase;
 import com.tom.atm22.data.Expense;
 import com.tom.atm22.data.ExpenseDatabase;
 
@@ -21,7 +24,8 @@ public class FinanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
-        final ExpenseDatabase database = Room.databaseBuilder(this, ExpenseDatabase.class,"expense.db").build();
+        final ExpenseDatabase database = ExpenseDatabase.getInstance(this);
+//        final ExpenseDatabase database = Room.databaseBuilder(this, ExpenseDatabase.class,"expense.db").build();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -49,6 +53,35 @@ public class FinanceActivity extends AppCompatActivity {
 //            }
 //        }).start();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_finance,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_expense_to_firebase){
+            final String userid = getSharedPreferences("atm",MODE_PRIVATE)
+                    .getString("USERID",null);
+            if(userid != null){
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                executorService.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<Expense> expenses =  ExpenseDatabase.getInstance(FinanceActivity.this).expenseDao().getAll();
+                        FirebaseDatabase.getInstance().getReference("users").child(userid).child("expenses")
+                                .setValue(expenses);
+                    }
+                });
+
+            }
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
