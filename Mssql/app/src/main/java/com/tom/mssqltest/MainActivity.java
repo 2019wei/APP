db3764548359eae2fdfb14bd9c477a7aeccc05dc
wei2019 <linkoko520@gmail.com>
2020-03-31 16:15:50 +0800

    0331-progress及最後一筆不會顯示

--- Mssql/app/src/main/java/com/tom/mssqltest/MainActivity.java
+++ Mssql/app/src/main/java/com/tom/mssqltest/MainActivity.java
@@ -3,6 +3,7 @@ package com.tom.mssqltest;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ArrayList<Data> list;
     private Listadapter listadapter;
+    private ProgressDialog progressD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +37,21 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.Listviews);
         listView.setAdapter(listadapter);
+
+        //ProgressDialog
+        progressD = new ProgressDialog(this);
+        progressD.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressD.setTitle(R.string.progressbar);
+        progressD.setProgressNumberFormat("");
+        progressD.setCancelable(false);
+        progressD.setIndeterminate(false);
+        progressD.setMax(100);
+
+
+
     }
     public void Bsql(View view){
+        progressD.show();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -64,20 +79,39 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
             list.clear();
-            pstmt = m_con.prepareStatement(sql);
+
+            pstmt = m_con.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+           //須設定ResultSet的Type ， 这是默认的，它是ResultSet.TYPE_FORWARD_ONLY，这意味着你只能使用rs.next()
             //
             ResultSet rs = pstmt.executeQuery();
+
+            rs.last();
+            int count = rs.getRow();
+            Log.d(TAG, "insert: "+count);
+            rs.beforeFirst();
+
+            int i = 1;
+
             while (rs.next()){
                 String id = rs.getString(1);
                 String name = rs.getString(2);
                 list.add(new Data(id,name));
+
+                Message m = new Message();
+                m.what = (int) (i/count) * 100;
+                handler2.sendMessage(m);
+                i++;
+
             }
             Log.d(TAG, "insert: 更新成功");
             pstmt.close();
         }catch (Exception e){
+            Log.e("Exception",e.getMessage());
             throw new Exception("更新失敗");
         }finally {
             handler.sendEmptyMessage(0);
+//            getHandler.sendEmptyMessage(0);
 //            runOnUiThread(new Runnable() {
 //                @Override
 //                public void run() {
@@ -85,18 +119,30 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            });
             m_con.close();
+            progressD.dismiss();
         }
 
     }
 
 
+
     private Handler handler =new Handler( new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
+            Log.d(TAG, "handleMessage: "+list.size());
             listadapter.notifyDataSetChanged();
             return true;
         }
     });
+
+    private Handler handler2 =new Handler( new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            progressD.setProgress(msg.what);
+            return true;
+        }
+    });
+
 
 
 
