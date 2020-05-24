9b8fac6aa860277d105def2e84aab5c759b71408
2019wei <linkoko520@gmail.com>
2020-05-24 16:45:15 +0800

    刪除資料夾

--- src/screens/StorageHelper.js
+++
@@ -1,5 +0,0 @@
-import{AsyncStorage} from 'react-native'
-
-
-export  const setMySetting =(key,value)=> AsyncStorage.setItem(key,value)
-export  const getMySetting =(key)=> AsyncStorage.getItem(key)
